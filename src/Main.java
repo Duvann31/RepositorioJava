54fdcc784647cca4718323b42d60bedb7a76ac5c
Duvan <sanchezduvan421@gmail.com>
2026-02-19 12:37:01 -0500

    Guardar cambios

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Duvan Felipe Sánchez Villalobos");
+        System.out.println("Gachancipa");
+        System.out.println("18 años");
+        System.out.println("Tengo dos mascotas y se llaman Manchas y Lilo");
     }
 }
